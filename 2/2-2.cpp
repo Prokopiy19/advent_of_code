2ece0dd8f2b00dc04c14980ad29bcd68746904f9
Prokopiy Danilov <danilovprokopiy@gmail.com>
2023-12-02 12:23:07 +0600

    cleaned up

--- 2/2-2.cpp
+++ 2/2-2.cpp
@@ -1,28 +1,26 @@
 #include <algorithm>
 #include <iostream>
 #include <fstream>
-#include <map>
 #include <sstream>
 #include <string>
+#include <unordered_map>
 
 int main()
 {
     int ans = 0;
     std::ifstream file("input.txt");
     for (std::string line; std::getline(file, line); ) {
-        line.push_back('.');
         std::istringstream record(line);
-        
+
         std::string skip;
         int id;
         record >> skip >> id >> skip;
 
-        std::map<std::string, int> maxs;
+        std::unordered_map<char, int> maxs;
         std::string color;
         for (int cnt; record >> cnt; ) {
             record >> color;
-            color.pop_back();
-            maxs[color] = std::max(maxs[color], cnt);
+            maxs[color[0]] = std::max(maxs[color[0]], cnt);
         }
         int power = 1;
         for (auto& [color, cnt] : maxs) {
--- 2/2.cpp
+++ 2/2.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
 #include <fstream>
+#include <unordered_map>
 #include <sstream>
 #include <string>
+
+std::unordered_map<char, int> maxs = { { 'r', 12 }, { 'g', 13 }, { 'b', 14 } };
 
 int main()
 {
     int ans = 0;
     std::ifstream file("input.txt");
     for (std::string line; std::getline(file, line); ) {
-        line.push_back('.');
         std::istringstream record(line);
 
         std::string skip;
         int id;
         record >> skip >> id >> skip;
-        
+
         bool correct = true;
         std::string color;
         for (int cnt; record >> cnt; ) {
             record >> color;
-            color.pop_back();
-            if (color == "red" && cnt > 12 || color == "green" && cnt > 13 || color == "blue" && cnt > 14) {
+            if (cnt > maxs[color[0]]) {
                 correct = false;
                 break;
             }
