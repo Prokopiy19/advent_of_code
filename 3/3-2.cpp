a94a90078227d370bef5c243e38a138f0b329efd
Prokopiy Danilov <danilovprokopiy@gmail.com>
2023-12-03 14:08:56 +0600

    Day 3

---
+++ 3/3-2.cpp
@@ -0,0 +1,78 @@
+#include <charconv>
+#include <cctype>
+#include <iostream>
+#include <iterator>
+#include <optional>
+#include <string>
+#include <utility>
+#include <vector>
+
+std::vector<std::string> scheme;
+int m, n;
+
+inline
+bool is_digit(int i, int j)
+{
+    return 0 <= i && i < m && 0 <= j && j < n && std::isdigit(scheme[i][j]);
+}
+
+struct Number {
+    int i, l, r;
+};
+
+std::optional<Number> get_number(int i, int j)
+{
+    if (0 <= i && i < m && 0 <= j && j < n && std::isdigit(scheme[i][j])) {
+        int l = j, r = j;
+        while (1 <= l && std::isdigit(scheme[i][l-1])) --l;
+        while (r < n && std::isdigit(scheme[i][r])) ++r;
+        std::optional<Number> ret;
+        ret.emplace(Number{i, l, r});
+        return ret;
+    }
+    else {
+        return {};
+    }
+}
+
+std::vector<Number> find_numbers(int i, int j)
+{
+    std::vector<Number> ret;
+    for (int ii = i - 1; ii <= i + 1; ++ii)
+        for (int jj = j - 1; jj <= j + 1; ) {
+            if (auto number = get_number(ii, jj)) {
+                ret.emplace_back(*number);
+                jj = number->r;
+            }
+            else {
+                ++jj;
+            }
+        }
+    return ret;
+}
+
+int main()
+{
+    for (std::string line; std::cin >> line; ) {
+        scheme.emplace_back(std::move(line));
+    }
+    m = scheme.size();
+    n = scheme[0].size();
+
+    long long ret = 0;
+    for (int i = 0; i < m; ++i)
+        for (int j = 0; j < n; ++j)
+            if (scheme[i][j] == '*') {
+                auto numbers = find_numbers(i, j);
+                if (numbers.size() == 2) {
+                    long long add = 1;
+                    for (const auto [ ii, l, r ] : numbers) {
+                        long long mul = 1;
+                        std::from_chars(scheme[ii].c_str() + l, scheme[ii].c_str() + r, mul);
+                        add *= mul;
+                    }
+                    ret += add;
+                }
+            }
+    std::cout << ret << '\n';
+}
---
+++ 3/3.cpp
@@ -0,0 +1,56 @@
+#include <charconv>
+#include <cctype>
+#include <iostream>
+#include <string>
+#include <utility>
+#include <vector>
+
+std::vector<std::string> scheme;
+int m, n;
+
+inline
+bool is_symbol(int i, int j)
+{
+    return 0 <= i && i < m && 0 <= j && j < n && !std::isdigit(scheme[i][j]) && scheme[i][j] != '.';
+}
+
+inline
+bool is_adjacent(int i, int j)
+{
+    return is_symbol(i-1, j-1) || is_symbol(i-1, j) || is_symbol(i-1, j+1) ||
+           is_symbol(i, j-1) || is_symbol(i, j+1) ||
+           is_symbol(i+1, j-1) || is_symbol(i+1, j) || is_symbol(i+1, j+1);
+}
+
+bool is_part(int i, int l, int r)
+{
+    for (int j = l; j < r; ++j) {
+        if (is_adjacent(i, j)) return true;
+    }
+    return false;
+}
+
+int main()
+{
+    for (std::string line; std::cin >> line; ) {
+        scheme.emplace_back(std::move(line));
+    }
+    m = scheme.size();
+    n = scheme[0].size();
+
+    long long ret = 0;
+    for (int i = 0; i < m; ++i)
+        for (int l = 0; l < n; ) {
+            int r = l;
+            while (r < n && std::isdigit(scheme[i][r])) {
+                ++r;
+            }
+            if (is_part(i, l, r)) {
+                long long add = 0;
+                std::from_chars(scheme[i].c_str() + l, scheme[i].c_str() + r, add);
+                ret += add;
+            }
+            l = (l == r) ? r + 1 : r;
+        }
+    std::cout << ret << '\n';
+}
