e6b94b3ed740c3b25cc7d9d24cc3f0813205c035
Prokopiy Danilov <danilovprokopiy@gmail.com>
2023-12-13 18:26:22 +0600

    tabs fix

--- 12/12.cpp
+++ 12/12.cpp
@@ -11,57 +11,57 @@ using namespace std;
 inline
 bool operational(char c)
 {
-	return c == '.' || c == '?';
+    return c == '.' || c == '?';
 }
 
 inline
 bool damaged(char c)
 {
-	return c == '#' || c == '?';
+    return c == '#' || c == '?';
 }
 
 int solve(string s, const vector<int>& nums)
 {
-	s = '.' + s + '.';
-	auto first = s.find('#');
-	first = (first == string::npos) ? s.size() : first;
-	vector<int> curr(s.size()), prev(s.size());
-	fill(curr.begin(), curr.begin() + first, 1);
-	for (int i = 0; i < nums.size(); ++i) {
-		swap(curr, prev);
-		fill(curr.begin(), curr.end(), 0);
-		for (int j = 0; j + nums[i] < s.size(); ++j)
-			if (all_of(s.begin() + j, s.begin() + j + nums[i], damaged))
-				for (int k = j+nums[i]; k < s.size() && operational(s[k]); ++k)
-					curr[k] += prev[j-1];
-	}
-	return curr.back();
+    s = '.' + s + '.';
+    auto first = s.find('#');
+    first = (first == string::npos) ? s.size() : first;
+    vector<int> curr(s.size()), prev(s.size());
+    fill(curr.begin(), curr.begin() + first, 1);
+    for (int i = 0; i < nums.size(); ++i) {
+        swap(curr, prev);
+        fill(curr.begin(), curr.end(), 0);
+        for (int j = 0; j + nums[i] < s.size(); ++j)
+            if (all_of(s.begin() + j, s.begin() + j + nums[i], damaged))
+                for (int k = j+nums[i]; k < s.size() && operational(s[k]); ++k)
+                    curr[k] += prev[j-1];
+    }
+    return curr.back();
 }
 
 signed main()
 {
-	int part_one = 0;
-	int part_two = 0;
-	for (string line; getline(cin, line); ) {
-		for (char& c : line) if (c == ',') c = ' ';
-		istringstream record(line);
-		
-		string s;
-		record >> s;
-		vector<int> nums;
-		for (int num; record >> num; ) {
-			nums.emplace_back(num);
-		}
-		part_one += solve(s, nums);
-		string s5;
-		vector<int> nums5;
-		for (int i = 0; i < 5; ++i) {
-			s5 += s + '?';
-			nums5.insert(nums5.end(), nums.cbegin(), nums.cend());
-		}
-		s5.pop_back();
-		part_two += solve(s5, nums5);
-	}
-	cout << part_one << '\n';
-	cout << part_two << '\n';
+    int part_one = 0;
+    int part_two = 0;
+    for (string line; getline(cin, line); ) {
+        for (char& c : line) if (c == ',') c = ' ';
+        istringstream record(line);
+        
+        string s;
+        record >> s;
+        vector<int> nums;
+        for (int num; record >> num; ) {
+            nums.emplace_back(num);
+        }
+        part_one += solve(s, nums);
+        string s5;
+        vector<int> nums5;
+        for (int i = 0; i < 5; ++i) {
+            s5 += s + '?';
+            nums5.insert(nums5.end(), nums.cbegin(), nums.cend());
+        }
+        s5.pop_back();
+        part_two += solve(s5, nums5);
+    }
+    cout << part_one << '\n';
+    cout << part_two << '\n';
 }
--- 13/13-2.cpp
+++ 13/13-2.cpp
@@ -6,60 +6,60 @@ using namespace std;
 
 int mismatch_rows(const vector<string>& b, int x, int y)
 {
-	int ret = 0;
-	for (int j = 0; j < b[x].size(); ++j)
-		ret += (b[x][j] != b[y][j]);
-	return ret;
+    int ret = 0;
+    for (int j = 0; j < b[x].size(); ++j)
+        ret += (b[x][j] != b[y][j]);
+    return ret;
 }
 
 int mismatch_cols(const vector<string>& b, int x, int y)
 {
-	int ret = 0;
-	for (int i = 0; i < b.size(); ++i)
-		ret += (b[i][x] != b[i][y]);
-	return ret;
+    int ret = 0;
+    for (int i = 0; i < b.size(); ++i)
+        ret += (b[i][x] != b[i][y]);
+    return ret;
 }
 
 bool hsymmetric(const vector<string>& b, int row)
 {
-	int m = 0;
-	for (int i = row, j = row + 1; 0 <= i && j < b.size(); --i, ++j) {
-		m += mismatch_rows(b, i, j);
-	}
-	if (m != 1) {
-		return false;
-	}
-	return true;
+    int m = 0;
+    for (int i = row, j = row + 1; 0 <= i && j < b.size(); --i, ++j) {
+        m += mismatch_rows(b, i, j);
+    }
+    if (m != 1) {
+        return false;
+    }
+    return true;
 }
 
 bool vsymmetric(const vector<string>& b, int col)
 {
-	int m = 0;
-	for (int i = col, j = col + 1; 0 <= i && j < b[0].size(); --i, ++j) {
-		m += mismatch_cols(b, i, j);
-	}
-	if (m != 1) {
-		return false;
-	}
-	return true;
+    int m = 0;
+    for (int i = col, j = col + 1; 0 <= i && j < b[0].size(); --i, ++j) {
+        m += mismatch_cols(b, i, j);
+    }
+    if (m != 1) {
+        return false;
+    }
+    return true;
 }
 
 int main()
 {
-	int part_one = 0;
-	for (string line; getline(cin, line); ) {
-		vector<string> b;
-		for ( ; !line.empty(); getline(cin, line)) {
-			b.emplace_back(std::move(line));
-		}
-		for (int i = 0; i + 1 < b.size(); ++i)
-			if (hsymmetric(b, i)) {
-				part_one += 100 * (i + 1);
-			}
-		for (int j = 0; j + 1 < b[0].size(); ++j)
-			if (vsymmetric(b, j)) {
-				part_one += j + 1;
-			}
-	}
-	cout << part_one << '\n';
+    int part_one = 0;
+    for (string line; getline(cin, line); ) {
+        vector<string> b;
+        for ( ; !line.empty(); getline(cin, line)) {
+            b.emplace_back(std::move(line));
+        }
+        for (int i = 0; i + 1 < b.size(); ++i)
+            if (hsymmetric(b, i)) {
+                part_one += 100 * (i + 1);
+            }
+        for (int j = 0; j + 1 < b[0].size(); ++j)
+            if (vsymmetric(b, j)) {
+                part_one += j + 1;
+            }
+    }
+    cout << part_one << '\n';
 }
