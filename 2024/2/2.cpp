398e1944ec35423b92f88212159b5a6fcb20654b
Prokopiy Danilov <danilovprokopiy@gmail.com>
2024-12-02 20:24:43 +0300

    clean up

--- 2024/2/2.cpp
+++ 2024/2/2.cpp
@@ -4,55 +4,64 @@
 #include <vector>
 #include <algorithm>
 
-bool checkSafety(const std::vector<int> &nums) {
-    for (int i = 0; i + 1 < ssize(nums); ++i) {
-        auto diff = nums[i + 1] - nums[i];
-        if (diff < 1 || 3 < diff) {
-            return false;
+bool checkDiff(int x, int y) {
+    auto diff = y - x;
+    return 1 <= diff && diff <= 3;
+}
+
+int checkSafety(const std::vector<int> &nums, int skip = -1) {
+    const int n = std::ssize(nums);
+    for (int i = 0; i + 1 < skip; ++i)
+        if (!checkDiff(nums[i], nums[i+1])) {
+            return i;
         }
+    if (0 <= skip - 1 && skip + 1 < n)
+        if (!checkDiff(nums[skip-1], nums[skip+1])) {
+            return skip;
+        }
+    for (int i = skip + 1; i + 1 < n; ++i)
+        if (!checkDiff(nums[i], nums[i+1])) {
+            return i;
+        }
+    return -1;
+}
+
+
+void solve(const std::vector<int> &nums, bool &s, bool &g) {
+    int mistake = checkSafety(nums);
+    if (mistake < 0) {
+        s = true;
+        g = true;
+        return;
     }
-    return true;
+    if (checkSafety(nums, mistake) < 0 || checkSafety(nums, mistake + 1) < 0) {
+        g = true;
+    }
 }
 
 int main() {
+
+    int silver = 0;
     int gold = 0;
     std::string line;
+    std::vector<int> nums;
     while (std::getline(std::cin, line)) {
+        nums.clear();
         std::istringstream record{line};
-        std::vector<int> nums;
         int num;
         while (record >> num) {
             nums.emplace_back(num);
         }
-        if (checkSafety(nums)) {
-            ++gold;
-            continue;
-        }
-        std::reverse(nums.begin(), nums.end());
-        if (checkSafety(nums)) {
-            ++gold;
-            continue;
-        }
-        std::reverse(nums.begin(), nums.end());
-        int tmp = nums.back();
-        nums.pop_back();
-        int swp = std::ssize(nums);
-        while (swp >= 0) {
-            if (checkSafety(nums)) {
-                ++gold;
-                break;
-            }
+        bool s = false;
+        bool g = false;
+        solve(nums, s, g);
+        if (!s) {
             std::reverse(nums.begin(), nums.end());
-            if (checkSafety(nums)) {
-                ++gold;
-                break;
-            }
-            std::reverse(nums.begin(), nums.end());
-            --swp;
-            if (swp >= 0) {
-                std::swap(nums[swp], tmp);
-            }
+            solve(nums, s, g);
         }
+        silver += s;
+        gold += g;
     }
-    std::cout << gold;
+    std::cout << "silver: " << silver << '\n';
+    std::cout << "gold: " << gold << std::endl;
 }
