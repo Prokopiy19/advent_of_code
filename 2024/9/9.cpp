a027a687cc395d0a4017ad54b716bf686c2ae411
Prokopiy Danilov <danilovprokopiy@gmail.com>
2024-12-09 08:44:16 +0300

    day 9 part 2

--- 2024/9/9.cpp
+++ 2024/9/9.cpp
@@ -7,34 +7,56 @@
 
 constexpr int kEmpty = -1;
 
+void print(const std::vector<int> &vec) {
+    // for (int i : vec) {
+    //     if (i != kEmpty) {
+    //         std::cout << i;
+    //     } else {
+    //         std::cout << '.';
+    //     }
+    // }
+    // std::cout << std::endl;
+}
+
 int main() {
-    long long silver = 0;
+    long long gold = 0;
     for (std::string line; std::getline(std::cin, line); ) {
         std::vector<int> filesystem;
-        // std::list<std::pair<int, int>> empty_spaces;
+        std::vector<std::pair<int, int>> blocks;
+        std::vector<std::pair<int, int>> empty_spaces;
         for (int i = 0; i < std::ssize(line); ++i) {
             if (i % 2 == 0) {
+                blocks.emplace_back(std::size(filesystem), line[i] - '0');
                 for (int t = 0; t < line[i] - '0'; ++t) {
                     filesystem.emplace_back(i  / 2);
                 }
             } else {
+                empty_spaces.emplace_back(std::ssize(filesystem), line[i] - '0');
                 for (int t = 0; t < line[i] - '0'; ++t) {
                     filesystem.emplace_back(kEmpty);
                 }
             }
         }
-        for (int i = 0; i < std::ssize(filesystem); ++i) {
-            if (filesystem[i] == kEmpty) {
-                filesystem[i] = filesystem.back();
-                filesystem.pop_back();
-            }
-            while (filesystem.back() == kEmpty) {
-                filesystem.pop_back();
+        print(filesystem);
+        for (int i = std::ssize(blocks) - 1; i > 0; --i) {
+            for (auto &[j, sz] : empty_spaces) {
+                if (j < blocks[i].first && sz >= blocks[i].second) {
+                    for (int x = 0; x < std::min(sz, blocks[i].second); ++x) {
+                        filesystem[j + x] = i;
+                        filesystem[blocks[i].first + x] = kEmpty;
+                    }
+                    blocks[i].first = j;
+                    j += blocks[i].second;
+                    sz -= blocks[i].second;
+                }
             }
         }
         for (int i = 0; i < std::ssize(filesystem); ++i) {
-            silver += filesystem[i] * i;
+            if (filesystem[i] != kEmpty) {
+                gold += filesystem[i] * i;
+            }
         }
+        print(filesystem);
     }
-    std::cout << silver << '\n';
+    std::cout << gold << '\n';
 }
